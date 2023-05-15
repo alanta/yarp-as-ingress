675fdc5e4b404c7bde17806130c26d04994df266
Marnix van Valen <m.vanvalen@4dotnet.nl>
2023-05-15 09:11:22 +0200

    ✨ Add load test app

---
+++ LoadTester/HMAC.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using NBomber.Http.CSharp;
+
+namespace LoadTester
+{
+    public static class HMAC
+    {
+        public static HttpRequestMessage WithHMACSignature(this HttpRequestMessage request, string keyId, string secret)
+        {
+            var timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds().ToString();
+            
+            var signature = CalculateHash("SECRET", $"GET{request.RequestUri}{timestamp}");
+
+            return request
+                .WithHeader("X-Request-Timestamp", timestamp)
+                .WithHeader("Authorization", "HMAC-SHA256 id=Test;signature=" + signature);
+        }
+
+        private static string CalculateHash(string key, string input)
+        {
+            var textBytes = Encoding.UTF8.GetBytes(input);
+            var keyBytes = Encoding.UTF8.GetBytes(key);
+
+            using var hash = new HMACSHA256(keyBytes);
+            var hashBytes = hash.ComputeHash(textBytes);
+
+            return Convert.ToBase64String(hashBytes);
+        }
+    }
+}
---
+++ LoadTester/Program.cs
@@ -0,0 +1,44 @@
+﻿using LoadTester;
+using NBomber.CSharp;
+using NBomber.Http.CSharp;
+
+// A basic test that hits both the frontend and api container
+//
+// You can play with the injection rates and intervals to see 
+// ACA scale the containers with traffic volume
+
+string baseUrl = args.FirstOrDefault() ?? "http://localhost:5204/";
+baseUrl = baseUrl.TrimEnd('/') + "/"; // ensure the url has a trailing slash
+
+var scenario = Scenario.Create("http_scenario", async context =>
+{
+    using var httpClient = new HttpClient();
+    var request =
+        Http.CreateRequest("GET", baseUrl)
+            .WithHeader("Accept", "text/html");
+
+    var response = await Http.Send(httpClient, request);
+    return response;
+})
+.WithoutWarmUp()
+.WithLoadSimulations(Simulation.Inject(rate: 10, interval: TimeSpan.FromSeconds(2), during: TimeSpan.FromMinutes(30)));
+
+var scenario2 = Scenario.Create("http_scenario2", async context =>
+{
+    using var httpClient = new HttpClient();
+    var url = baseUrl + "api/hello/stranger";
+    
+    var request =
+        Http.CreateRequest("GET", url )
+            .WithHMACSignature("Test", "SECRET")
+            .WithHeader("Accept", "text/html");
+
+    var response = await Http.Send(httpClient, request);
+    return response;
+})
+.WithoutWarmUp()
+.WithLoadSimulations(Simulation.Inject(rate: 10, interval: TimeSpan.FromSeconds(2), during: TimeSpan.FromMinutes(30)));
+
+NBomberRunner
+    .RegisterScenarios(scenario, scenario2)
+    .Run();
